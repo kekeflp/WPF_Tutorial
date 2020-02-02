39af4acc428f41f00df052cb804d2d55f4964ab7
hasee <kekeflp@hotmail.com>
2020-02-02 18:52:44 +0800

    补充

--- TreeViewsAndValueConverters/TreeViewsAndValueConverters/MainWindow.xaml.cs
+++ TreeViewsAndValueConverters/TreeViewsAndValueConverters/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,26 @@ namespace TreeViewsAndValueConverters
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Directory.GetLogicalDrives() 的作用是拿到本机的磁盘数量
+            // 此处用for循环指定数量也可以
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+
+                // 添加磁盘头和全路径
+                item.Header = drive;
+                item.Tag = drive;
+                 
+                // 添加一个空项目
+                item.Items.Add(null);
+
+                // 添加到主视图中
+                FoldView.Items.Add(item);
+            } 
         }
     }
 }
