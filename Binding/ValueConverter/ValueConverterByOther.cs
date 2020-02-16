03b362c4cd2af2e9e14f3c10791ad37f10336f91
hasee <kekeflp@hotmail.com>
2020-02-16 16:14:25 +0800

    增加DataBinding示例

---
+++ Binding/ValueConverter/ValueConverterByOther.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace ValueConverter
+{
+    public class ValueConverterByOther : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double height = (double)value;
+            if (double.TryParse(parameter.ToString(), out double d))
+            {
+                return height * d;
+            }
+            else
+            {
+                throw new Exception($"parameter converter error: {parameter} .");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
